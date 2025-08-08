f92187fa640b70e1251e94b9ab38c3caac054924
tushanthnath-hub <tushanthnath@gmail.com>
2025-08-08 14:35:55 +0530

    21349

--- home/views.py
+++ home/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+import requests
 
-# Create your views here.
+def home(request):
+    api_url = "http://127.0.0.1:8000/api/menu/"  # Your API endpoint
+    try:
+        response = requests.get(api_url)
+        menu_items = response.json() if response.status_code == 200 else []
+    except requests.exceptions.RequestException:
+        menu_items = []  # Fallback if API is down
+
+    return render(request, 'home.html', {'menu_items': menu_items})

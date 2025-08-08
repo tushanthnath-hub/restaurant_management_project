72588a0ed3925cbd5a1dfbbb348f5dfaa675df16
tushanthnath-hub <tushanthnath@gmail.com>
2025-08-08 14:39:43 +0530

    21349

--- account/views.py
+++ account/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import MenuItem  # replace with your actual model name
 
-# Create your views here.
+def home(request):
+    menu_items = MenuItem.objects.all()
+    return render(request, 'home.html', {'menu_items': menu_items})

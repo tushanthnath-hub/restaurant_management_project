c5fe64bd21b46c01dc2b6ca79246b7f69abbde8b
tushanthnath-hub <tushanthnath@gmail.com>
2025-08-08 14:38:16 +0530

    21349

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import UserProfile, MenuItem  # Replace MenuItem with your actual model name
 
-# Register your models here.
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'name', 'email', 'phone_number')
+    search_fields = ('name', 'email', 'phone_number')
+    list_filter = ('user',)
+
+@admin.register(MenuItem)
+class MenuItemAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'description')
+    search_fields = ('name', 'description')
+    list_filter = ('price',)

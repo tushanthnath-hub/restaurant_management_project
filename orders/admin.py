ff6fe69f8ed737dbf24870dbef0a8e801460c9a8
tushanthnath-hub <tushanthnath@gmail.com>
2025-08-08 14:41:56 +0530

    21350

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import UserProfile, MenuItem, Order, OrderItem
 
-# Register your models here.
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'name', 'email', 'phone_number')
+    search_fields = ('name', 'email', 'phone_number')
+
+@admin.register(MenuItem)
+class MenuItemAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'description')
+    search_fields = ('name', 'description')
+    list_filter = ('price',)
+
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    extra = 1
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('id', 'customer', 'total_amount', 'status', 'created_at')
+    list_filter = ('status', 'created_at')
+    search_fields = ('customer__username',)
+    inlines = [OrderItemInline]

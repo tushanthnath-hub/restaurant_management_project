27d053c0ad6ec177667ae06ab04beb65079b4179
tushanthnath-hub <tushanthnath@gmail.com>
2025-08-08 14:47:41 +0530

    21351

--- products/admin.py
+++ products/admin.py
@@ -1,11 +1,24 @@
 from django.contrib import admin
-from .models import *
+from .models import MenuItem, Order, OrderItem
+
+# Custom admin for menu items
+@admin.register(MenuItem)
+class MenuItemAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'description')
+    search_fields = ('name', 'description')
+    list_filter = ('price',)
 
 
-# Custom Admins
-class ItemAdmin(admin.ModelAdmin):
-    list_display = ['item_name','item_price','created_at']
+# Inline order items in the order view
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    extra = 1
 
 
-# Register your models here.
-admin.site.register(Item,ItemAdmin)
+# Custom admin for orders
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('id', 'customer', 'total_amount', 'status', 'created_at')
+    list_filter = ('status', 'created_at')
+    search_fields = ('customer__username',)
+    inlines = [OrderItemInline]

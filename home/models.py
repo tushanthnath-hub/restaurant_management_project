e6a06a63beab9d62716e0cc3b4284cd5095e48ff
tushanthnath-hub <tushanthnath@gmail.com>
2025-08-08 14:11:10 +0530

    21349

--- home/models.py
+++ home/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.shortcuts import render
 
-# Create your models here.
+def home(request):
+    # Replace with your actual API endpoint
+    api_url = "http://127.0.0.1:8000/api/menu/"
+    response = requests.get(api_url)
+    
+    if response.status_code == 200:
+        menu_items = response.json()
+    else:
+        menu_items = []
+
+    return render(request, 'home.html', {'menu_items': menu_items})

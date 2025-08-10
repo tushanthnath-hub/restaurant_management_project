3fff0d4184e24940ca46ea940232ec651f950848
tushanthnath-hub <tushanthnath@gmail.com>
2025-08-10 09:41:41 +0530

    27247

--- restaurant_management/__pycache__/views.py
+++ restaurant_management/__pycache__/views.py
@@ -1,11 +1,9 @@
 from django.shortcuts import render
 
-def menu_list(request):
-    # Temporary hardcoded menu items
-    menu_items = [
-        {"name": "Margherita Pizza", "price": 299},
-        {"name": "Paneer Tikka", "price": 249},
-        {"name": "Pasta Alfredo", "price": 349},
-        {"name": "Veg Burger", "price": 199},
-    ]
-    return render(request, 'menu_list.html', {'menu_items': menu_items})
+def contact_us(request):
+    contact_info = {
+        "phone": "+91 98765 43210",
+        "email": "contact@tastybites.com",
+        "address": "123, Food Street, Hyderabad, India"
+    }
+    return render(request, 'contact.html', {'contact_info': contact_info})

469b068541d4b7666de85f50525ccb5681ca5a1a
tushanthnath-hub <tushanthnath@gmail.com>
2025-08-20 20:51:39 +0530

    35577

--- restaurant_management/model.py
+++ restaurant_management/model.py
@@ -1,7 +1,10 @@
-from django import forms
-from .models import ContactSubmission
+from django.db import models
 
-class ContactForm(forms.ModelForm):
-    class Meta:
-        model = ContactSubmission
-        fields = ['name', 'email']
+class RestaurantLocation(models.Model):
+    address = models.CharField(max_length=255)
+    city = models.CharField(max_length=100)
+    state = models.CharField(max_length=100)
+    zip_code = models.CharField(max_length=10)
+
+    def __str__(self):
+        return f"{self.address}, {self.city}, {self.state} {self.zip_code}"

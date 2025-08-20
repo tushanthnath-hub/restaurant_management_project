12a86c75cbe29bfcbe80aab1e724be5fe12911f7
tushanthnath-hub <tushanthnath@gmail.com>
2025-08-20 20:55:58 +0530

    35574

--- restaurant_management/model.py
+++ restaurant_management/model.py
@@ -1,10 +1,10 @@
 from django.db import models
 
-class RestaurantLocation(models.Model):
-    address = models.CharField(max_length=255)
-    city = models.CharField(max_length=100)
-    state = models.CharField(max_length=100)
-    zip_code = models.CharField(max_length=10)
+class MenuItem(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    image = models.ImageField(upload_to='menu_images/', blank=True, null=True)
 
     def __str__(self):
-        return f"{self.address}, {self.city}, {self.state} {self.zip_code}"
+        return self.name
